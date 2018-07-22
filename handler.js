dcdbad3f9f0c1cd4ac46a255e187a89490e0ad38
Faris <faris@Dietmar-Stretzs-iMac.local>
2018-07-22 14:01:52 +0200

    Initial Commit

---
+++ handler.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports.hello = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+      input: event,
+    }),
+  };
+
+  callback(null, response);
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+};
+
+module.exports.create = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+  
+  connectToDatabase()
+    .then(() => {
+      Note.create(JSON.parse(event.body))
+        .then(note => callback(null, {
+          statusCode: 200,
+          body: JSON.stringify(note)
+        }))
+        .catch(err => callback(null, {
+          statusCode: err.statusCode || 500,
+          headers: { 'Content-Type': 'text/plain' },
+          body: 'Could not create the note.'
+        }));
+    });
+};
+
